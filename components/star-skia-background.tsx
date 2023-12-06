f27f68ea497c77d1a30ed2e0e12d88aed5a6cc68
Paul <colandreapaul@gmail.com>
2023-12-05 21:41:43 -0500

    fix performance on skia bg

--- components/star-skia-background.tsx
+++ components/star-skia-background.tsx
@@ -1,9 +1,10 @@
-import { Canvas, Circle, Easing, runSpring, runTiming, useValue } from '@shopify/react-native-skia';
+import { Canvas, Circle, Easing, Line, runTiming, useValue } from '@shopify/react-native-skia';
 import React, { useEffect } from 'react';
-import { Dimensions, StyleSheet, View, Text } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
-const NUM_STARS = 500;
+const NUM_STARS = 250;
+const NUM_LAYERS = 2; // Number of layers
 
 const starColors = [
   '#FFFFFF', // White (common color)
@@ -21,9 +22,7 @@ const starColors = [
   '#FF9999', // Red (rarer color)
 ];
 
-const randomIncrement = (maxValue) => {
-  return (Math.random() - 0.5) * 2 * maxValue;
-};
+const randomIncrement = (maxValue) => (Math.random() - 0.5) * 2 * maxValue;
 
 const animateStar = (posX, posY, maxDistance, movementDuration) => {
   const newPosX = posX.current + randomIncrement(maxDistance);
@@ -49,18 +48,43 @@ const animateOpacity = (opacity, start, end, duration, onComplete) => {
   );
 };
 
-const Star = () => {
-  const posX = useValue(Math.random() * width);
-  const posY = useValue(Math.random() * height);
-  const opacity = useValue(Math.random() * 0.5 + 0.5);
-  const size = Math.random() * 2 + 1;
+const SparklingStar = ({ x, y, size, color, opacity }) => {
+  const numLines = 2 + Math.floor(Math.random() * 3); // 2 to 4 lines for variation
+  const lines = [];
 
-  const color = starColors[Math.floor(Math.random() * starColors.length)];
+  for (let i = 0; i < numLines; i++) {
+    const angle = Math.random() * 2 * Math.PI; // Random angle for each line
+    const lineLength = size * (0.5 + Math.random() * 0.5); // Randomize line length
+    lines.push({
+      p1: { x, y },
+      p2: {
+        x: x + lineLength * Math.cos(angle),
+        y: y + lineLength * Math.sin(angle),
+      },
+    });
+  }
+
+  return lines.map((line, index) => (
+    <Line
+      key={index}
+      p1={line.p1}
+      p2={line.p2}
+      color={color}
+      strokeWidth={Math.random() * 0.5 + 0.5} // Randomize stroke width for a more subtle effect
+      opacity={opacity * Math.random() * 0.5 + 0.5} // Varying opacity for a twinkling effect
+    />
+  ));
+};
+
+const LayeredStar = ({ star }) => {
+  const posX = useValue(star.x);
+  const posY = useValue(star.y);
+  const opacity = useValue(star.opacity);
+  const size = star.size;
 
   useEffect(() => {
-    const maxDistance = 20; // Max distance a star can move in any direction
+    const maxDistance = 20 * star.layerFactor; // Max distance a star can move in any direction
     const movementDuration = 8000 + Math.random() * 4000; // Increased duration for slower movement
-
     animateStar(posX, posY, maxDistance, movementDuration);
 
     // Twinkling effect
@@ -79,16 +103,52 @@ const Star = () => {
     }
   }, []);
 
-  return <Circle cx={posX} cy={posY} r={size / 2} color={color} opacity={opacity} />;
+  if (star.shape === 'sparkling') {
+    return (
+      <SparklingStar
+        x={posX.current}
+        y={posY.current}
+        size={star.size}
+        color={star.color}
+        opacity={star.opacity.current}
+      />
+    );
+  }
+
+  return <Circle cx={posX} cy={posY} r={size / 2} color={star.color} opacity={opacity} />;
 };
+
+// Function to generate random stars for a given layer
+const generateStarsForLayer = (layerIndex) => {
+  return Array.from({ length: NUM_STARS / NUM_LAYERS }).map(() => {
+    const layerFactor = (layerIndex + 1) / NUM_LAYERS;
+    const color = starColors[Math.floor(Math.random() * starColors.length)];
+    const shape = Math.random() < 0.2 ? 'sparkling' : 'circle'; // 20% chance to be a sparkling star
+
+    return {
+      x: Math.random() * width,
+      y: Math.random() * height,
+      size: (Math.random() * 3 + 1) * layerFactor,
+      opacity: (Math.random() * 0.5 + 0.5) * layerFactor,
+      layerFactor,
+      color,
+      shape,
+    };
+  });
+};
+
+// Creating stars for each layer
+const layers = Array.from({ length: NUM_LAYERS }).map((_, index) => generateStarsForLayer(index));
 
 const StarBackgroundSkia = ({ children }) => {
   return (
     <View style={styles.container}>
       <Canvas style={styles.canvas}>
-        {Array.from({ length: NUM_STARS }).map((_, index) => (
-          <Star key={index} />
-        ))}
+        {layers.map((layer, layerIndex) =>
+          layer.map((star, starIndex) => (
+            <LayeredStar key={`layer-${layerIndex}-star-${starIndex}`} star={star} />
+          ))
+        )}
       </Canvas>
       <View style={styles.childrenContainer}>{children}</View>
     </View>
