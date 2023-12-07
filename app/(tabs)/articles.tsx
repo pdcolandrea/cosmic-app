eda2bc2b53fa078915174a90d81d5117580fff05
Paul <colandreapaul@gmail.com>
2023-12-07 17:22:31 -0500

    add base fact screen

--- app/(tabs)/articles.tsx
+++ app/(tabs)/articles.tsx
@@ -1,20 +1,28 @@
 import { Text, View } from 'react-native';
 
 import { useDailyFact } from '../../hooks/use-random-fact';
+import { SkiaNonLayedStarBackground } from '../../components/star-skia-background-standard';
+import Animated, { FadeInDown } from 'react-native-reanimated';
 
 export default function TabTwoScreen() {
   const fact = useDailyFact();
-  console.log(fact);
 
   return (
-    <View className={styles.container}>
-      <Text className={styles.title}>{fact.fact}</Text>
-    </View>
+    <SkiaNonLayedStarBackground>
+      <View className={styles.container}>
+        <Text style={{ fontFamily: 'Rubik_700Bold' }} className="text-3xl text-white">
+          Did you know...
+        </Text>
+        <Animated.Text entering={FadeInDown.delay(500)} className={styles.title}>
+          {fact.fact.replace('Did you know', '')}
+        </Animated.Text>
+      </View>
+    </SkiaNonLayedStarBackground>
   );
 }
 
 const styles = {
   container: `items-center flex-1 justify-center`,
   separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
-  title: `text-xl font-bold text-center`,
+  title: `text-3xl font-sans font-bold text-center text-white`,
 };
